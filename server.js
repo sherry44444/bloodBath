91c413f94cd7493c4d70d7906e55cb16f063c2ef
Hoang Xuan Lam <toi.di.tjm.toj@gmail.com>
2019-10-24 21:16:48 +0700

    elenventh

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use(cors());
 //middleware
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
-app.use(express.static(path.join((__dirname, "./client/public"))));
+app.use(express.static(path.join((__dirname, "client/public"))));
 
 //react form bi mat xxx-urlrecoded nen data no gui di o dang json chu ko bi ma hoa nua nen khi post data dang raw (json) (tu frontedn dang dung react) thi phai dung middleware o duoi
 app.use(express.json());
