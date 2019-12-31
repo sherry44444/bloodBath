f74f3088488b634cd87a91383126e7c772ccac64
Hoang Xuan Lam <toi.di.tjm.toj@gmail.com>
2019-12-31 21:06:40 +0700

    fix server

--- server.js
+++ server.js
@@ -31,15 +31,20 @@ app.use(cors());
 //middleware
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
-app.use(express.static("client/build"));
-// app.get("*", function(req, res) {
-//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// });
 
 //react form bi mat xxx-urlrecoded nen data no gui di o dang json chu ko bi ma hoa nua nen khi post data dang raw (json) (tu frontedn dang dung react) thi phai dung middleware o duoi
 app.use(express.json());
 app.use("/api/donations", donationRouter);
 app.use("/api/users", userRouter);
+
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = Number(process.env.PORT || 8888);
 
