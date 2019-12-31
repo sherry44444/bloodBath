c44c1dd7b401847c75aab7aee969ea7f47e96d45
Hoang Xuan Lam <toi.di.tjm.toj@gmail.com>
2019-12-31 19:59:23 +0700

    fix server

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
 app.use(express.static("client/build"));
 app.get("*", function(req, res) {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "/index.html"));
 });
 
 //react form bi mat xxx-urlrecoded nen data no gui di o dang json chu ko bi ma hoa nua nen khi post data dang raw (json) (tu frontedn dang dung react) thi phai dung middleware o duoi
