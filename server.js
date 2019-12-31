090f0436874034caa381ce9a8a5a0a85d33b43a1
Hoang Xuan Lam <toi.di.tjm.toj@gmail.com>
2019-12-31 16:42:10 +0700

    ad func to catch all routes

--- server.js
+++ server.js
@@ -32,9 +32,9 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
 app.use(express.static("client/build"));
-// app.get("*", function(req, res) {
-//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// });
+app.get("*", function(req, res) {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 //react form bi mat xxx-urlrecoded nen data no gui di o dang json chu ko bi ma hoa nua nen khi post data dang raw (json) (tu frontedn dang dung react) thi phai dung middleware o duoi
 app.use(express.json());
